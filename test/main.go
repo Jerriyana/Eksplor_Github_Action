ca2507cbf02d003e20424bffc24409e6acef2456
Jerriyana <131117771+Jerriyana@users.noreply.github.com>
2024-03-15 06:36:10 +0700

    Update main.go

--- test/main.go
+++ test/main.go
@@ -4,33 +4,33 @@ import (
 	"fmt"
 )
 
-// Fungsi untuk mencetak pesan ke konsol
-func printMessage() {
-	fmt.Println("Halo, ini adalah contoh pesan!")
-}
+// // Fungsi untuk mencetak pesan ke konsol
+// func printMessage() {
+// 	fmt.Println("Halo, ini adalah contoh pesan!")
+// }
 
-// Fungsi untuk melakukan perulangan menggunakan for
-func loopingExample() {
-	fmt.Println("Contoh perulangan:")
-	for i := 0; i < 5; i++ {
-		fmt.Println("Iterasi ke", i)
-	}
-}
+// // Fungsi untuk melakukan perulangan menggunakan for
+// func loopingExample() {
+// 	fmt.Println("Contoh perulangan:")
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println("Iterasi ke", i)
+// 	}
+// }
 
-// Fungsi untuk menunjukkan penggunaan if-else
-func ifElseExample() {
-	fmt.Println("Contoh penggunaan if-else:")
-	x := 10
-	if x > 5 {
-		fmt.Println("x lebih besar dari 5")
-	} else {
-		fmt.Println("x tidak lebih besar dari 5")
-	}
-}
+// // Fungsi untuk menunjukkan penggunaan if-else
+// func ifElseExample() {
+// 	fmt.Println("Contoh penggunaan if-else:")
+// 	x := 10
+// 	if x > 5 {
+// 		fmt.Println("x lebih besar dari 5")
+// 	} else {
+// 		fmt.Println("x tidak lebih besar dari 5")
+// 	}
+// }
 
 // Fungsi utama untuk menjalankan contoh fungsi-fungsi di atas
 func main() {
-	printMessage()
-	loopingExample()
-	ifElseExample()
+	// printMessage()
+	// loopingExample()
+	// ifElseExample()
 }
