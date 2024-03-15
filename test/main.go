4af22f497c293d5c677e48d94d0787544293e281
Jerriyana <131117771+Jerriyana@users.noreply.github.com>
2024-03-15 09:07:39 +0700

    Update main.go

--- test/main.go
+++ test/main.go
@@ -2,35 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-// // Fungsi untuk mencetak pesan ke konsol
-// func printMessage() {
-// 	fmt.Println("Halo, ini adalah contoh pesan!")
-// }
-
-// // Fungsi untuk melakukan perulangan menggunakan for
-// func loopingExample() {
-// 	fmt.Println("Contoh perulangan:")
-// 	for i := 0; i < 5; i++ {
-// 		fmt.Println("Iterasi ke", i)
-// 	}
-// }
-
-// // Fungsi untuk menunjukkan penggunaan if-else
-// func ifElseExample() {
-// 	fmt.Println("Contoh penggunaan if-else:")
-// 	x := 10
-// 	if x > 5 {
-// 		fmt.Println("x lebih besar dari 5")
-// 	} else {
-// 		fmt.Println("x tidak lebih besar dari 5")
-// 	}
-// }
-
-// Fungsi utama untuk menjalankan contoh fungsi-fungsi di atas
 func main() {
-	// printMessage()
-	// loopingExample()
-	// ifElseExample()
+	// Variabel yang tidak digunakan
+	_ := "Ini variabel yang tidak digunakan"
+
+	// Panggil fungsi yang tidak ada
+	result := calculate(10, 5)
+
+	// Print hasil
+	fmt.Println("Hasil perhitungan:", result)
+}
+
+// Fungsi untuk menghitung hasil penjumlahan dan perpangkatan
+func calculate(a, b int) int {
+	penjumlahan := add(a, b)
+	perpangkatan := math.Pow(float64(a), float64(b))
+	return int(penjumlahan) + perpangkatan
+}
+
+// Fungsi untuk penjumlahan
+func add(a, b int) int {
+	return a + b
+}
+
+// Fungsi untuk mencetak pesan sambutan
+func welcomeMessage(name string) {
+	fmt.Println("Selamat datang,", name)
 }
